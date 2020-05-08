c79f4fca9e3c6b2870f077d568262bf76eb3c57b
footlessbird <arslongavitabrevis0818@gmail.com>
2020-05-08 16:00:43 -0700

    heroku test 5

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -8,10 +8,15 @@
 //     createProxyMiddleware("/api/**", { target: "http://localhost:5000" })
 //   );
 // }
+if (process.env.NODE_ENV !== "production") {
+  const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-module.exports = function (app) {
-  app.use(createProxyMiddleware("/auth", { target: "http://localhost:5000" }));
-  app.use(createProxyMiddleware("/polls", { target: "http://localhost:5000" }));
-};
+  module.exports = function (app) {
+    app.use(
+      createProxyMiddleware("/auth", { target: "http://localhost:5000" })
+    );
+    app.use(
+      createProxyMiddleware("/polls", { target: "http://localhost:5000" })
+    );
+  };
+}
