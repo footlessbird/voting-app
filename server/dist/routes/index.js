bdb8ed5dba4627abc97977d7550a8a9c559631f3
footlessbird <arslongavitabrevis0818@gmail.com>
2020-05-07 23:33:15 -0700

    dist removed

--- server/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var auth_1 = __importDefault(require("./auth"));
-var poll_1 = __importDefault(require("./poll"));
-exports.default = { auth: auth_1.default, poll: poll_1.default };
