2e66b22e4b5899b5c1d184792f8c8d5c0fa1dcea
footlessbird <arslongavitabrevis0818@gmail.com>
2020-05-10 01:19:13 -0700

    이건 됨?

--- server/index.ts
+++ server/index.ts
@@ -69,11 +69,19 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/../client/build")));
   console.log("production logic");
 
+  /*
   app.get("*", (req, res) => {
     // res.sendFile(path.resolve(clientBuildDir, "index.html"));
-    res.sendFile(path.join(__dirname, "/../client/build", "index.html"));
+    // res.sendFile(path.join(__dirname, "/../client/build", "index.html"));
+    res.sendFile(path.resolve(clientDistDir, 'index.html'));
   });
 }
+*/
+
+app.use(express.static('client/build'));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+})
 
 app.listen(PORT, () => {
   console.log("env is ", process.env.NODE_ENV);
